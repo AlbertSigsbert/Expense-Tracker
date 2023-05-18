6628f1e835733059ef12d097df81a0e33460cc42
AlbertSigsbert <ishengomaalbert@gmail.com>
2023-05-18 14:20:55 +0300

    Create expense list item

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
+
+  final Expense expense;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.airplanemode_off),
+                    const SizedBox(width: 8),
+                    Text(expense.date.toString())
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
