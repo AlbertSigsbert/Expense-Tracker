d95cfe8604a16fff7e17d817e43718fd3ccbc05b
AlbertSigsbert <ishengomaalbert@gmail.com>
2023-05-19 06:16:37 +0300

    Adding a modal sheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -25,16 +25,23 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, 
+        builder: (ctx) => const Text('Modal Bottom Sheet')
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.add)
-         )
-         ],
+          IconButton(
+              onPressed: _openAddExpenseOverlay, 
+              icon: const Icon(Icons.add)
+              )
+        ],
       ),
       body: Column(
         children: [
