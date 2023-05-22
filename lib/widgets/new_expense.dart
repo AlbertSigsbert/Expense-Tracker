4f0edad8521ba38319b0e20e8f0a6e0b37253bff
AlbertSigsbert <ishengomaalbert@gmail.com>
2023-05-22 10:30:07 +0300

    Add amount numberInput & cancel btn

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
-  
   const NewExpense({super.key});
 
   @override
@@ -12,30 +11,44 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(
-        children:  [
+      child: Column(children: [
         TextField(
           controller: _titleController,
           maxLength: 50,
-          decoration:const InputDecoration(label: Text('Title')),
+          decoration: const InputDecoration(label: Text('Title')),
+        ),
+        TextField(
+          controller: _amountController,
+          keyboardType: TextInputType.number,
+          decoration:
+              const InputDecoration(prefixText: '\$ ', label: Text('Amount')),
         ),
         Row(
           children: [
-            ElevatedButton(onPressed: (){
-              print(_titleController);
-            }, child: const Text('Save Expense!'))
+            TextButton(
+              onPressed: () {},
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(_titleController.text);
+                print(_amountController.text);
+              },
+              child: const Text('Save Expense!'),
+            ),
           ],
         )
       ]),
